f7c378780fe8472c54dd0eabfbaf3aab0151ef79
diksha sahu <diksha143@gmail.com>
2023-08-16 11:17:46 +0530

    latest update

--- suppi.py
+++ suppi.py
@@ -1,8 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/", methods=["GET"])
-def root():
-    return "Hello DITISS!!"
-     
+from flask import *  
+app = Flask(__name__)  
+  
+@app.route('/login',methods = ['POST'])  
+def login():  
+      uname=request.form['uname']  
+      passwrd=request.form['pass']  
+      if uname=="ayush" and passwrd=="google":  
+          return "Welcome %s" %uname  
+   
+if __name__ == '__main__':  
+   app.run(debug = True)       
  
 app.run(host="0.0.0.0",port=4050, debug=True)
